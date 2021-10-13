b2370cf9e1aee98791f5250521225d4f8957bc86
jake-hong <hongse21@gmail.com>
2021-10-13 16:04:19 +0900

    Add implementation test

---
+++ implementation/02.time.py
@@ -0,0 +1,12 @@
+# 3이 하나라도 들어 있는 경우 세기 
+
+h = int(input())
+
+count = 0 
+
+for i in range(h+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                count+=1
+print(count) 
---
+++ implementation/03.royal_night.py
@@ -0,0 +1,16 @@
+data = input() # a1 
+
+row = int(data[1])
+column = int(ord(data[0])) - int(ord('a')) + 1 
+
+steps = [(-2,-1),(-1,-2),(1,-2),(2,-1),(2,1),(1,2),(-1,2),(-2,1)]
+result = 0 
+
+for step in steps:
+    next_row = row + step[0]
+    next_col = column + step[1]
+
+    if next_row >=1 and next_row <=8 and next_col >=1 and next_row <= 8:
+        result += 1
+
+print(result)
